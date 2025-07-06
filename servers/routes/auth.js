ae169fa88ed5bef9763ba56affd372ec9539d2bf
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-07-06 11:29:20 +0530

    Changed the server side

--- servers/routes/auth.js
+++ servers/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const User = require('../models/User');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
